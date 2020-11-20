c2e493f342cf3dfb00799121c41badb931bc6da6
Bobi <borko17@hotmail.com>
2020-11-20 20:38:47 +0200

    Modification

--- hobbies.js
+++ hobbies.js
@@ -1,9 +1,11 @@
 const hobbies = ["jogging","cooking","diving","swimming","reading"];
+
 exports.getHobbies = () => {
-  return hobbies;
+    return hobbies;
 }
+
 exports.getHobby = id => {
-  if(id<1 || id > hobbies.length)
-    return null;
-  return hobbies[id-1];
+    if(id<1 || id > hobbies.length)
+        return null;
+    return hobbies[id-1];
 }
